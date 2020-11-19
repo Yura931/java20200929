62ff1676da25d839996628709345357c1660b1d8
admin <admin@DESKTOP-TMT9OH0>
2020-11-19 17:48:07 +0900

    20201119baekjoon

---
+++ src/baekjoon/Num10951.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Num10951 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A+B);
+		}
+		sc.close();
+	}
+}
