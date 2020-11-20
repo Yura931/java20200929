f82a8888fa8d5b6bfa86d8bff803f60f8e5ba0a8
Yura931 <dbfk931@naver.com>
2020-11-20 19:56:11 +0900

    20201120codingbat

---
+++ src/codingbat/warmup_2/N10_stringYak.java
@@ -0,0 +1,9 @@
+package codingbat.warmup_2;
+
+public class N10_stringYak {
+
+	public static String stringYak(String str) {
+		return str.replace("yak", "");
+	}
+	
+}	
---
+++ src/codingbat/warmup_2/No09_array667.java
@@ -0,0 +1,25 @@
+package codingbat.warmup_2;
+
+public class No09_array667 {
+	
+	public static int array667(int[] nums) {
+		int arr1[] = null;
+		int arr2[] = null;
+		int cnt = 0;
+		
+		for (int i = 0; i <nums.length - 1; i++) {
+			int n1 = arr1[i];
+			int n2 = arr2[i+1];
+		
+		if (n1 == 6 && n2 == 7 || n1 == 6 && n2 == 6) {
+			cnt++;
+		}
+		}
+		return cnt;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
+
+
